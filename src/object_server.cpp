46ac12d24c44422318adaf71d64e4f36616c6f5a
Konstantin Lassnig <konstantin.lassnig@student.tugraz.at>
2015-12-17 14:13:26 +0100

    added the other actions too

--- src/object_server.cpp
+++ src/object_server.cpp
@@ -7,7 +7,9 @@ namespace stage_ros
 ObjectServer::ObjectServer(ros::NodeHandle nh, StageNode* stage) :
         node_(nh),
         stage_(stage),
-        create_action_server_(node_, "/stage/create", boost::bind(&ObjectServer::executeCreate, this, _1), false)
+        create_action_server_(node_, "/stage/create", boost::bind(&ObjectServer::executeCreate, this, _1), false),
+        move_action_server_(node_, "/stage/move", boost::bind(&ObjectServer::executeMove, this, _1), false),
+        remove_action_server_(node_, "/stage/remove", boost::bind(&ObjectServer::executeRemove, this, _1), false)
 {
   create_action_server_.start();
 }
@@ -31,32 +33,27 @@ void ObjectServer::executeCreate(const stage_ros::createGoalConstPtr&)
 
   create_action_server_.setSucceeded(res);
 
-//    tedusar_exploration_msgs::MoveToPathResult res;
-//
-//    res.move_to_path.header.frame_id = "map";
-//
-//    tf::Stamped<tf::Pose> robot_pose_tf;
-//    costmap_2d_ros_move_to_->getRobotPose(robot_pose_tf);
-//
-//    geometry_msgs::PoseStamped start_pose;
-//    tf::poseStampedTFToMsg(robot_pose_tf, start_pose);
-//
 //    if(!hector_move_to_planner_->makePlan(start_pose, goal->goto_point, res.move_to_path.poses))
 //    {
 //        move_to_server_.setAborted(res, "exporation of hector failed");
 //        return;
 //    }
 //
-//    res.move_to_path.header.stamp = ros::Time::now();
-//
 //    if(move_to_server_.isPreemptRequested())
 //    {
 //        move_to_server_.setPreempted();
 //        return;
 //    }
-//
-//    plan_pub_.publish(res.move_to_path);
-//    move_to_server_.setSucceeded(res);
+}
+
+void ObjectServer::executeMove(const stage_ros::moveGoalConstPtr&)
+{
+
+}
+
+void ObjectServer::executeRemove(const stage_ros::removeGoalConstPtr&)
+{
+
 }
 
 }  // namespace stage_ros
